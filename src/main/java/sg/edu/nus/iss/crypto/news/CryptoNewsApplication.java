ed330e834b6f5b4727e017886958d861effb8528
lkas96 <kwek_lawson@hotmail.com>
2024-12-11 14:49:02 +0800

    Spring Template + 2 Maven Repo

---
+++ src/main/java/sg/edu/nus/iss/crypto/news/CryptoNewsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.crypto.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/crypto/news/CryptoNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.crypto.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
